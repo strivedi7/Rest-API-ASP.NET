d1a865b5caa9e6e89444ae937c7d7ccc560ef392
Saurabh Trivedi <trivedi@qualcomm.com>
2019-03-26 17:29:04 -0700

    final

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PersonAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,31 +10,57 @@ namespace PersonAPI.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        List<Person> people = new List<Person>();
+ 
+
+        
+        
+        public ValuesController()
         {
-            return new string[] { "value1", "value2" };
+            people.Add(new Person { firstName = "saurabh", lastName = "Trivedi", id = 1 });
+            people.Add(new Person { firstName = "Arzoo", lastName = "Patel", id = 2 });
+        }
+
+        [Route("api/values/firstNames")]
+        [HttpGet]
+        public List<string> GetFirstNames (){
+            List<string> result = new List<string>();
+            foreach (var p in people)
+            {
+                result.Add(p.firstName);
+            }
+            return result;
+        }
+
+        [Route("api/values/firstNames/{id}")]
+        [HttpGet]
+        public string GetFirstNames(int id)
+        {
+          
+            return people.Where(x => x.id == id).FirstOrDefault().firstName;
+        }
+        // GET api/values
+        public List<Person> Get()
+        {
+            return people;
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Person Get(int id)
         {
-            return "value";
+            return people.Where(x => x.id == id).FirstOrDefault();
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public void Post(Person val)
         {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
+            people.Add(val);
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            people.Remove(people.Where(x => x.id == id).FirstOrDefault());
         }
     }
 }
---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonAPI.Models
+{
+    public class Person
+    {
+        
+
+        public int id { get; set; } = 0;
+        public String firstName { get; set; } = "";
+        public String lastName { get; set; } = "";
+    }
+}
